af4175e0e6137b35df2e4283076470fb45c7bacd
Бордуненко Игорь <bordunenko@it-up.org>
2022-12-01 15:06:32 +0300

    Готовая среда для разработки и тестирования нового функционала. (Для обучения)

---
+++ platformForTraining/functions/code.php
@@ -0,0 +1,5 @@
+<?
+
+use \Classes\Request\Request;
+use \Classes\Request\HttpClient;
+
---
+++ platformForTraining/index.php
@@ -0,0 +1,24 @@
+<?
+spl_autoload_register();
+
+require_once __DIR__ . '/functions/functions.php';
+require_once __DIR__ . '/functions/debug.php';
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>training-oop</title>
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <? require_once('functions/code.php'); ?>
+</body>
+
+</html>
+<?
