eeda3711799563548092365ed708113dcc1b4593
Бордуненко Игорь <bordunenko@it-up.org>
2022-12-01 14:17:40 +0300

    Класс для безопасной работы с массивом $_REQUEST

---
+++ classes/request/request.php
@@ -0,0 +1,57 @@
+<?
+
+namespace Classes\Request;
+
+/**
+ * Класс для безопасной работы с массивом $_REQUEST
+ * Класс реализует паттерн Singleton
+ */
+class Request
+{
+    private static $instance = null;
+    private static $storage; // переменная хранящая данные GET и POST
+
+    private function __construct()
+    {
+    }
+    private function __clone()
+    {
+    }
+    private function __wakeup()
+    {
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$storage = self::cleanInput($_REQUEST);
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    public function __get($name)
+    {
+        if (isset(self::$storage[$name])) return self::$storage[$name];
+    }
+
+    // очистка данных от опасных символов
+    private static function cleanInput($data)
+    {
+        if (is_array($data)) {
+            $cleaned = [];
+            foreach ($data as $key => $value) {
+                $cleaned[$key] = self::cleanInput($value);
+            }
+            return $cleaned;
+        }
+        return trim(htmlspecialchars($data, ENT_QUOTES));
+    }
+
+    // возвращаем содержимое хранилища
+    public function getRequestEntries()
+    {
+        return self::$storage;
+    }
+}
