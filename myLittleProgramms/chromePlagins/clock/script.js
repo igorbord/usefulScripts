79038fe84ad68d9adf450e50a7a89574d9d8a31a
Бордуненко Игорь <bordunenko@it-up.org>
2022-12-16 19:37:13 +0300

    Добавил календарь в расширение хрома

--- myLittleProgramms/chromePlagins/clock/script.js
+++ myLittleProgramms/chromePlagins/clock/script.js
@@ -1,25 +1,32 @@
-const deg = 6;
-const hr = document.querySelector('#hr');
-const sc = document.querySelector('#sc');
+window.addEventListener('DOMContentLoaded', () => {
 
-setInterval(() => {
-    let day = new Date();
-    let hh = day.getHours() * 30;
-    let mm = day.getMinutes() * deg;
-    let ss = day.getSeconds() * deg;
+    const deg = 6;
+    const hr = document.querySelector('#hr');
+    const sc = document.querySelector('#sc');
 
-    hr.style.transform = `rotateZ(${(hh) + (mm / 12)}deg)`;
-    mn.style.transform = `rotateZ(${mm}deg)`;
-    sc.style.transform = `rotateZ(${ss}deg)`;
+    showClock()
+    setInterval(showClock, 1000)
 
+    function showClock() {
+        let day = new Date();
+        let hh = day.getHours() * 30;
+        let mm = day.getMinutes() * deg;
+        let ss = day.getSeconds() * deg;
+
+        hr.style.transform = `rotateZ(${(hh) + (mm / 12)}deg)`;
+        mn.style.transform = `rotateZ(${mm}deg)`;
+        sc.style.transform = `rotateZ(${ss}deg)`;
+    }
+
+    let date = new Date();
     const weekDaysName = {
-        0: "Воскресенье",
-        1: "Понедельник",
-        2: "Вторник",
-        3: "Среда",
-        4: "Четверг",
-        5: "Пятница",
-        6: "Суббота",
+        0: "Вс",
+        1: "Пн",
+        2: "Вт",
+        3: "Ср",
+        4: "Чт",
+        5: "Пт",
+        6: "Сб",
     }
     const monthName = {
         0: "Января",
@@ -36,6 +43,42 @@ setInterval(() => {
         11: "Декабря",
     }
 
-    // ${day.getHours()}:${day.getMinutes()}:${day.getSeconds()}
-    document.querySelector('.date').innerText = `${weekDaysName[day.getDay()]}, ${day.getDate()} ${monthName[day.getMonth()]}(${day.getMonth() + 1}) ${day.getFullYear()} `;
+    document.querySelector('.date').innerText = `${weekDaysName[date.getDay()]}, ${date.getDate()} ${monthName[date.getMonth()]}(${date.getMonth() + 1}) ${date.getFullYear()} `;
+
+    const calendar = document.querySelector('.calendar .table')
+    let row = document.createElement('div');
+    row.classList.add('row')
+    for (let c = 1; c <= 7; c++) {
+        let dayNum = c == 7 ? weekDaysName[0] : weekDaysName[c]
+        row.insertAdjacentHTML('beforeend', `<div class="col day-name">${dayNum}</div>`)
+    }
+    calendar.insertAdjacentElement('beforeend', row)
+
+    let flag = false
+    let calendarDay = new Date(date.getFullYear(), date.getMonth(), 1);
+    let prevDay = calendarDay.getDate()
+    for (let r = 0; r < 6; r++) {
+        row = document.createElement('div');
+        row.classList.add('row')
+        for (let c = 1; c <= 7; c++) {
+            if (!flag) {
+                let weekDay = calendarDay.getDay() == 0 ? 6 : calendarDay.getDay()
+                if (weekDay == c) {
+                    flag = true
+                }
+            }
+
+            if (flag && calendarDay.getDate() >= prevDay) {
+                let active = calendarDay.getDate() == date.getDate() ? 'active' : ''
+                let holiday = calendarDay.getDay() == 0 || calendarDay.getDay() == 6 ? 'holiday' : ''
+                row.insertAdjacentHTML('beforeend', `<div class="col ${active} ${holiday}">${calendarDay.getDate()}</div>`)
+                prevDay = calendarDay.getDate()
+                calendarDay.setTime(calendarDay.getTime() + 86400000)
+            } else {
+                row.insertAdjacentHTML('beforeend', `<div class="col"></div>`)
+            }
+        }
+        calendar.insertAdjacentElement('beforeend', row)
+        if (calendarDay.getDate() < prevDay) break;
+    }
 })
