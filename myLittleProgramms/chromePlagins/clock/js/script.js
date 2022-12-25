2abd1958f739e50daac126765230fde129de038d
Бордуненко Игорь <bordunenko@it-up.org>
2022-12-25 22:32:46 +0300

    Clock. Изменен формат даты

--- myLittleProgramms/chromePlagins/clock/js/script.js
+++ myLittleProgramms/chromePlagins/clock/js/script.js
@@ -29,21 +29,21 @@ window.addEventListener('DOMContentLoaded', () => {
         6: "Сб",
     }
     const monthName = {
-        0: "Января",
-        1: "Февраля",
-        2: "Марта",
-        3: "Апреля",
-        4: "Мая",
-        5: "Июня",
-        6: "Июля",
-        7: "Августа",
-        8: "Сентября",
-        9: "Октября",
-        10: "Ноября",
-        11: "Декабря",
+        0: "Январь",
+        1: "Февраль",
+        2: "Март",
+        3: "Апрель",
+        4: "Май",
+        5: "Июнь",
+        6: "Июль",
+        7: "Август",
+        8: "Сентябрь",
+        9: "Октябрь",
+        10: "Ноябрь",
+        11: "Декабрь",
     }
 
-    document.querySelector('.date').innerText = `${weekDaysName[date.getDay()]}, ${date.getDate()} ${monthName[date.getMonth()]}(${date.getMonth() + 1}) ${date.getFullYear()} `;
+    document.querySelector('.date').innerText = `${monthName[date.getMonth()]} ( ${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()} )`;
 
     const calendar = document.querySelector('.calendar .table')
     let row = document.createElement('div');
