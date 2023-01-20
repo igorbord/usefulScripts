4074214bf2a72a306ec1ffe33a2bc0281666ad73
igorbord <igorbord@mail.ru>
2023-01-20 14:30:53 +0300

    Исправлен класс Config

--- Classes/Config.class.php
+++ Classes/Config.class.php
@@ -1,6 +1,12 @@
 <?
 
+namespace Classes;
+
+use Exception;
+
 /**
+ * Класс для взаимодействия с файлом конфига
+ * Класс реализует паттерн Singleton
  * Для работы класса нужно предварительно объявить константу CFG_PATH,
  * в которой указан путь к json файлу конфига
  */
@@ -8,11 +14,12 @@ class Config
 {
     private static $instance = null;
     private static $cfg = null;
+    private $cfgChild = null;
 
     private function __construct($cfg = null)
     {
         if (!is_null($cfg)) {
-            self::$cfg = $cfg;
+            $this->cfgChild = $cfg;
         }
     }
     private function __clone()
@@ -40,22 +47,26 @@ class Config
 
     public function __get($val)
     {
-        if (is_array(self::$cfg[$val])) {
+        if (is_array(self::$cfg[$val]))
             return new self(self::$cfg[$val]);
-        }
-        return self::$cfg[$val];
-    }
 
-    public function setToken($value): object
-    {
-        self::$cfg['token'] = $value;
-        if (!file_exists(CFG_PATH)) throw new Exception('Config file not found');
-        file_put_contents(CFG_PATH, json_encode(self::$cfg), LOCK_EX);
-        return $this;
+        if (isset($this->cfgChild) && isset($this->cfgChild[$val]))
+            return $this->cfgChild[$val];
+        elseif (!isset($this->cfgChild) && isset(self::$cfg[$val]))
+            return self::$cfg[$val];
+        else
+            throw new Exception('variable "' . $val . '" is not exist');
     }
 
     public function get()
     {
+        if (isset($this->cfgChild))
+            return $this->cfgChild;
         return self::$cfg;
+    }
+
+    // Запрещаю изменять данные
+    public function __set($key, $val)
+    {
     }
 }
