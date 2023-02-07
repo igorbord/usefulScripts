05c465b0017588fa6bb4f79641d89c505b8e0bfd
igorbord <igorbord@mail.ru>
2023-02-07 19:21:53 +0300

    Доработан класс Config

--- Classes/Config.class.php
+++ Classes/Config.class.php
@@ -1,9 +1,5 @@
 <?
 
-namespace Classes;
-
-use Exception;
-
 /**
  * Класс для взаимодействия с файлом конфига
  * Класс реализует паттерн Singleton
@@ -15,17 +11,20 @@ class Config
     private static $instance = null;
     private static $cfg = null;
     private $cfgChild = null;
+    private $pathKeys = [];
 
-    private function __construct($cfg = null)
+    private function __construct($cfg = null, $pathKeys = null, $key = null)
     {
         if (!is_null($cfg)) {
             $this->cfgChild = $cfg;
+            $this->pathKeys = $pathKeys;
+            $this->pathKeys[] = $key;
         }
     }
-    private function __clone()
+    public  function __clone()
     {
     }
-    private function __wakeup()
+    public  function __wakeup()
     {
     }
 
@@ -47,12 +46,12 @@ class Config
 
     public function __get($val)
     {
-        if (isset(self::$cfg[$val]) && is_array(self::$cfg[$val]))
-            return new self(self::$cfg[$val]);
+        if (!isset($this->cfgChild) && isset(self::$cfg[$val]) && is_array(self::$cfg[$val]))
+            return new self(self::$cfg[$val], $this->pathKeys, $val);
 
         if (isset($this->cfgChild) && isset($this->cfgChild[$val])) {
             if (is_array($this->cfgChild[$val]))
-                return new self($this->cfgChild[$val]);
+                return new self($this->cfgChild[$val], $this->pathKeys, $val);
             return $this->cfgChild[$val];
         } elseif (!isset($this->cfgChild) && isset(self::$cfg[$val]))
             return self::$cfg[$val];
@@ -60,21 +59,33 @@ class Config
             throw new Exception('variable "' . $val . '" is not exist');
     }
 
-    public function get()
+    public function get(): mixed
     {
         if (isset($this->cfgChild))
             return $this->cfgChild;
         return self::$cfg;
     }
 
-    // Запрещаю изменять данные
-    public function __set($key, $val)
+    public function __set($key, $val): void
     {
+        $pathKeys = array_merge($this->pathKeys, [$key]);
+
+        // Проверка, что данный параметр можно изменять
+        if (!in_array(implode('->', $pathKeys), [
+            'key1->key3->key4',
+        ])) throw new Exception("Куда полез!? Нельзя переписывать конфиг!");
+
+        $newArray = [];
+        for ($i = count($pathKeys) - 1; $i >= 0; $i--) {
+            $newArray = [$pathKeys[$i] => $pathKeys[$i] == $key ? $val : $newArray];
+        }
+
+        self::$cfg = array_replace_recursive(self::$cfg, $newArray);
+        $this->saveConfig();
     }
 
-    public function setCategoryNextVersion($version): object
+    private function saveConfig(): object
     {
-        self::$cfg['enaza']['categoryNextVersion'] = $version;
         $result = file_put_contents(CFG_PATH, json_encode(self::$cfg), LOCK_EX);
         if ($result === false) throw new Exception('Error save config');
         if (!file_exists(CFG_PATH)) throw new Exception('Config file not found');
